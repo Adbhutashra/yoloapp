7a00d5ae8be4254d3dab28997e0dcf268ab1cbce
Adbhut Ashra <ashra.adbhut@gmail.com>
2025-01-21 22:26:48 +0530

    code navigation

--- App.js
+++ App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import PaymentScreen from './PaymentScreen';
+import 'react-native-gesture-handler';
+
+const Stack = createStackNavigator();
 
 function App() {
     return (
-        <PaymentScreen />
+        <NavigationContainer>
+            <Stack.Navigator screenOptions={{ headerShown: false }}>
+                <Stack.Screen 
+                    name="Payment" 
+                    component={PaymentScreen} 
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
     );
 }
+
 export default App;
